bf319a421fe67287d199f6644bde9053f0e380ed
EVERTON DIOGO FERREIRA MELO <everton_d_melo@estudante.sesisenai.org.br>
2025-11-10 22:18:04 -0300

    Trabalho SA

--- ong-doapet/script.js
+++ ong-doapet/script.js
@@ -1,8 +1,8 @@
-function entrar(){
-    let nome = prompt("email:")
-    let senha = prompt("Senha:")
+// function entrar(){
+//     let nome = prompt("email:")
+//     let senha = prompt("Senha:")
 
-    if(nome == "admin" $$ senha == "admin"){
-        window.location.href = 'index.html'
-    }
-}
+//     if(nome == "admin" $$ senha == "admin"){
+//         window.location.href = 'index.html'
+//     }
+// }
